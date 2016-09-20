4451a50a01e1c7efe3ed4e67bd3d67fdbfe8a540
Sam Hadden <shadden@u.northwestern.edu>
2016-09-20 13:28:56 -0500

    Functionality for running batch jobs on Quest added

--- semianalytic_megno.py
+++ semianalytic_megno.py
@@ -1,6 +1,6 @@
 from ctypes import *
 import numpy as np
-
+from argparse import ArgumentParser
 import os
 who =os.popen("whoami") 
 if who.readline().strip() =='samuelhadden':
@@ -28,27 +28,88 @@ class libwrapper(object):
 		
 		
 if __name__=="__main__":
-	w = libwrapper()
-	n1range = np.linspace(1.5*(1-0.007),1.5*(1+0.007),10)
-	n2range = np.linspace(1./1.5/(1+0.007),1./1.5/(1-0.007),10)
 
-	tfin = 2*np.pi*1e+4
-	dt = 2*np.pi/30.
-	m1=1e-5
-	m2=1e-5
-	
+	parser = ArgumentParser(description='Run a grid of simulations and compute the MEGNO')
+	parser.add_argument('-N','--Ngrid',metavar='N',type=int,default=10,help='Number of grid points in each dimension')
+	parser.add_argument('-C','--checkpoints',metavar='N',type=int,default=0,help='Number of checkpoints')
+	parser.add_argument('--restart', default=False, action='store_true', help='continue a previously-existing run')
+	parser.add_argument('-T','--Time',metavar='t',type=float,default=5.0e4,help='Lengh of simulations')
+	parser.add_argument('-F','--infile',metavar='<FILE>',default='input.txt',help='Text file with period ranges')
+
+	args = parser.parse_args()
+	Ngrid = args.Ngrid
+	simLength = args.Time * 2 * np.pi
+	infile = args.infile
+	checkpoints = args.checkpoints + 1
+	restart = args.restart
+
+	with open(infile,'r') as fi:
+		planetMass1,planetMass2 = map(float, fi.readline().split() )
+		n1min,n1max = map(float, fi.readline().split() )
+		n2min,n2max = map(float, fi.readline().split() )
+		e1,w1  = map(float, fi.readline().split() )
+		e2,w2  = map(float, fi.readline().split() )
+		etp,wtp  = map(float, fi.readline().split() )
+		theta1,theta2,thetatp=map(float,fi.readline().split())
+		Mtp  = map(float, fi.readline().split() )[0]
+
+	print "Sim. length:", simLength
+	print "Planet 1: m=%.1e , e=%.3f , w= %.3f deg. , theta= %.3f deg."%(planetMass1,e1,w1,theta1)
+	print "Planet 2: m=%.1e , e=%.3f , w= %.3f deg. , theta= %.3f deg."%(planetMass2,e2,w2,theta2)
+	print "Test particle: m=%.1e , e=%.3f , w= %.3f deg. , theta= %.3f deg."%(Mtp,etp,wtp,thetatp)
+
+	w1 = np.pi * w1 / 180.
+	w2 = np.pi * w2 / 180.
+	wtp = np.pi * wtp / 180.
+	theta1 = np.pi * theta1 / 180.
+	theta2 = np.pi * theta2 / 180.
+	thetatp = np.pi * thetatp / 180.
+
+
+	rHill = ( np.max([ planetMass1,planetMass2 ]) /3.)**(1./3.)
+
+	w = libwrapper()
 	def f(pars):
 		n1,n2 = pars
+		n1 = n1 / 2. / np.pi
+		n2 = n2 / 2. / np.pi
 		period = n1
 		omega2 = n2 / n1
-		return w.MEGNO_Integration(tfin,dt,period,0.,m1,m2,omega2)
-	
+		return w.MEGNO_Integration(simLength, 2*np.pi/30. ,period,0.,planetMass1,planetMass2,omega2)
+
+
+	par_d1 =  np.linspace(n1min,n1max,Ngrid+1)[:-1] 
+	par_d2 =  np.linspace(n2min,n2max,Ngrid+1)[:-1]
+
+	parameters = []
+	for d2 in par_d2:
+	    for d1 in par_d1:
+	        parameters.append((d1,d2))
+
+	parameters = np.array(parameters)
 	from rebound.interruptible_pool import InterruptiblePool
+	
 	pool = InterruptiblePool()
-	parameters = [ (n1,n2) for n1 in n1range for n2 in n2range ]
-	results = pool.map(f , parameters)
+	if restart:
+		import re
+		import glob
+		checkpointfiles = glob.glob("checkpoint_*.dat")
+		exprn = re.compile("checkpoint_(\d+)")
+		checkPointNumbers = [ int(re.search(exprn,x).group(1)) for x in checkpointfiles ]
+		cMax = np.max(checkPointNumbers)
+		results = np.fromfile("checkpoint_%d.dat"%cMax).tolist()
+		print "Restarting from checkpoint file checkpoint_%d.dat..."%cMax
+		sys.stdout.flush()
+	else:
+		results = []
+		cMax = -1
 
+	for c in range(checkpoints)[cMax+1:]:
+		index_low = c*len(parameters)/checkpoints
+		index_high = (c+1)*len(parameters)/checkpoints
+		results = results + pool.map(f , parameters[index_low:index_high])
+		np.array(results).tofile("checkpoint_%d.dat"%c)
+	
 	resultsArr = np.hstack((np.array(parameters),np.array(results).reshape(-1,1) ))
-	resultsArr.tofile("results.dat")
-
-
+	np.save("MEGNO_RESULTS.npy",resultsArr)
+	resultsArr.tofile("MEGNO_RESULTS.dat")
