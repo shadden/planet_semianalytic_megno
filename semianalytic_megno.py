3ede58e854ea82d279605460fecf334c3a0d585e
Samuel Hadden <shadden1107@gmail.com>
2017-04-11 11:27:52 -0500

    minor changes

--- semianalytic_megno.py
+++ semianalytic_megno.py
@@ -31,13 +31,6 @@ def get_fcoeff_ratio(pratio):
 # Resonant terms to include
 def full_resonance_list(resJ,order):
 	return [[resJ,order,i] for i in range(0,order+1)]
-Farey1=[(0, 1) , (1, 1)]
-Farey2=[(0, 1), (1, 2), (1, 1)]
-Farey3=[(0, 1), (1, 3), (1, 2), (2, 3), (1, 1)]
-Farey4=[(0, 1), (1, 4), (1, 3), (1, 2), (2, 3), (3, 4), (1, 1)]
-Farey5=[(0, 1), (1, 5), (1, 4), (1, 3), (2, 5), (1, 2), (3, 5), (2, 3), (3, 4), (4, 5), (1, 1)]
-Farey6=[(0, 1), (1, 6), (1, 5), (1, 4), (1, 3), (2, 5), (1, 2), (3, 5), (2, 3), (3, 4), (4, 5), (5, 6), (1, 1)]
-Farey7=[(0, 1), (1, 7), (1, 6), (1, 5), (1, 4), (2, 7), (1, 3), (2, 5), (3, 7), (1, 2), (4, 7), (3, 5), (2, 3), (5, 7), (3, 4), (4, 5), (5, 6), (6, 7), (1, 1)]
 
 def Farey_Sequence_N(n):
 	# Farey sequence function lifted from Wikipedia
@@ -311,7 +304,7 @@ if __name__=="__main__":
 
 	res1=[]
 	res2=[]
-	for i,o in Farey7:
+	for i,o in Farey_Sequence_N(7):
 		j1= o*(3) + i
 		res1= res1 + full_resonance_list(j1,o)
 		res2= res2 + full_resonance_list(j1,o)
